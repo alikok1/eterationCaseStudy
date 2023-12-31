e9595e8ce35749b6fa054e58035cb9f8c5a2c26f
alikok1 <alikok7@gmail.com>
2023-12-31 23:08:29 +0300

    update components

--- eterationcase/src/components/routesComponent.js
+++ eterationcase/src/components/routesComponent.js
@@ -1,16 +1,22 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { Component } from "react";
+import {Switch,Route,} from "react-router-dom";
 import * as ROUTES from "../constants/routePaths";
 import HomePage from "../pages/homePage";
 import ProductDetailPage from "../pages/productDetailPage";
 
-const RoutesComponent = () => {
-    return (
-        <Routes>
-            <Route path={ROUTES.HOME_PAGE} element={<HomePage />} />
-            <Route path={ROUTES.PRODUCT_DETAIL_PAGE} element={<ProductDetailPage />} />
-        </Routes>
-    );
+class RoutesComponent extends Component {
+
+    render() {
+        return (
+            <div>
+
+                <Switch>
+                    <Route exact path={ROUTES.HOME_PAGE} component={HomePage}/>
+                    <Route exact path={ROUTES.PRODUCT_DETAIL_PAGE} component={ProductDetailPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default RoutesComponent;
